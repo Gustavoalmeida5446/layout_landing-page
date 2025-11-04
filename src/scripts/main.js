6136e4b3f08fb1d1d4def07dc761dea805dfd12c
gustavo <gustavoalmeida5446@gmail.com>
2025-11-04 10:02:49 -0300

    feat: finish project layout and styles

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,12 @@
-'use strict';
+const botaoAbrir = document.getElementById('menu-button');
+const botaoFechar = document.getElementById('close-menu-button');
+const menuVermelho = document.querySelector('.menu-mobile');
+
+function alternarMenu() {
+  menuVermelho.classList.toggle('is-open');
+
+  document.body.classList.toggle('no-scroll');
+}
+
+botaoAbrir.addEventListener('click', alternarMenu);
+botaoFechar.addEventListener('click', alternarMenu);
---
+++ src/scripts/main.jsx
@@ -0,0 +1,9 @@
+'use strict';
+
+export function header() {
+  return (
+    <header>
+      <h1>Welcome to My Website</h1>
+    </header>
+  );
+}
